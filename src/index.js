7529196b52237620202ff6eef593dbd4a65c2b55
Gregg Housh <gregg@macguffinventures.com>
2020-09-29 03:32:53 +0000

    Moved to alpinejs for the mobile menu. Built out hero, middle, bottom, and footer sections. Brought in font awesome. Added buttons for all social accounts.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./styles.css";
 import "alpinejs";
+import "@fortawesome/fontawesome-free/css/brands.min.css";
+import "@fortawesome/fontawesome-free/css/solid.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+
 console.log("Launching The Gregg Housh Show!");
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   },
   purge: ["src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitch: "#6441a5",
+        facebook: "#39569c",
+        twitter: "#26a7de",
+        instagram: "#F7FAFC",
+        youtube: "#ff0100",
+        picton: "#49aaf1",
+      },
+    },
   },
   variants: {},
   plugins: [],
