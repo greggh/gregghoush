dd2e4ae015914ae5ed1971e9f5b608f04858ef33
Gregg Housh <gregg@macguffinventures.com>
2023-08-22 21:21:50 -0400

    Starting the redesign.

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -21,7 +21,13 @@ module.exports = {
         instagram: "#F7FAFC",
         youtube: "#ff0100",
         picton: "#49aaf1",
+        neonpink: "#cf3775",
+        neonblue: "#5fc5e9",
       },
+      gradientColorStops: (theme) => ({
+        neonpink: "#cf3775",
+        neonblue: "#5fc5e9",
+      }),
       boxShadow: {
         blue: "0 4px 14px 0 rgba(19, 51, 81, 0.39)",
       },
